32c65e12ffe86f401fbecac885f46ac27c65c408
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-05-12 23:10:29 +0900

    setting.sh: Fix check the ufw status
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- setting.sh
+++ setting.sh
@@ -1,6 +1,14 @@
-sudo apt-get -y install mysql-server
-sudo apt-get -y install libmysqlclient-dev
-sudo ufw enable
-sudo ufw allow 22/tcp
-sudo ufw allow 3306/tcp
-sudo ufw allow 55555/tcp
+#!/bin/bash
+
+sudo apt-get -y install mysql-server libmysqlclient-dev
+
+STATUS=`sudo ufw status | grep inactive`
+
+if [ -z "$STATUS" ]; then
+	echo "Currently ufw enabled"
+	echo "allow port: (3306, 55555)"
+	sudo ufw allow 3306/tcp
+	sudo ufw allow 55555/tcp
+else
+	echo "Currently ufw disabled"
+fi
